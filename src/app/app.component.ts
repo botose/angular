14a68379f0206cc6de8bc9544daca57666bce941
Elemer Botos <v-ebotos@expedia.com>
2017-04-25 22:57:37 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {Hero} from './hero';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: `app/myFirstTemplate.html`,
+})
+export class AppComponent  {
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
+  heroes = [
+    new Hero(0, 'Solomon'),
+    new Hero(1, 'Peter'),
+    new Hero(2, 'Judas')
+  ];
+}
