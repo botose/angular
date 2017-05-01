a06f34b5f6fbfbb06fbcd2cd35ec2bf4417702d4
Elemér Botos <Elemer_Botos@epam.com>
2017-05-01 20:33:09 +0200

    remove unneccessary files

--- src/app/cities/mock-cities.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var City_1 = require("./City");
-exports.CITIES = [
-    new City_1.City(0, 'Budapest', 1000, 'City'),
-    new City_1.City(1, 'Belgrade', 1000, 'City')
-];
-//# sourceMappingURL=mock-cities.js.map
